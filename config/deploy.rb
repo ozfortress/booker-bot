a62b45ed7def10d97633c89b93543a4963c0e455
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-01-26 02:48:42 +1100

    Added capistrano configs for deploying to staging

---
+++ config/deploy.rb
@@ -0,0 +1,56 @@
+# config valid only for current version of Capistrano
+lock '3.7.1'
+
+set :application, 'BookerBot'
+set :repo_url, 'git@github.com:ozfortress/BookerBot.git'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/opt/booker_bot'
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+append :linked_files, 'config/secrets.json'
+# append :linked_files, 'config/database.yml', 'config/secrets.yml'
+
+# Default value for linked_dirs is []
+append :linked_dirs, 'node_modules'
+# append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+
+# Default value for default_env is {}
+# set :default_env, { path: '/opt/ruby/bin:$PATH' }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+namespace :deploy do
+  desc 'Fetch Dependencies'
+  task :npm_install do
+    on roles(:app) do
+      within current_path do
+        execute :npm, :install
+      end
+    end
+  end
+
+  desc 'Restart Application'
+  task :restart do
+    on roles(:app) do
+      execute :sudo, :systemctl, :restart, fetch(:service_name)
+    end
+  end
+
+  after :publishing, :npm_install
+  after :npm_install, :restart
+end
