6a38513773b28cedc9eda5e65279ce82082a2459
BenjaminSchaaf <ben.schaaf@gmail.com>
2017-02-13 20:07:13 +1100

    Cleaned up settings

--- src/settings.js
+++ src/settings.js
@@ -2,5 +2,4 @@ module.exports = {
     ssc: require("../config/ssc.json"),
     discord: require("../config/discord.json"),
     secrets: require("../config/secrets.json"),
-    demos: require("../config/demos.json"),
 }
